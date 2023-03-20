1117e9c279044212cfa3c103153b7aea8128bf29
nstylo <niklas.stylianou@cloudflight.io>
2023-03-20 21:03:46 +0100

    Fix some typing and give players ids

--- onitama_project/onitama_server/server.py
+++ onitama_project/onitama_server/server.py
@@ -2,9 +2,11 @@ import random
 import sys
 from pathlib import Path
 from typing import Dict, List
-from uuid import uuid4
+from uuid import UUID, uuid4
 
+import uvicorn
 from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect
+from pydantic import BaseModel
 
 # Add the parent directory to the Python path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
@@ -14,57 +16,66 @@ from onitama_engine import Color, Onitama
 app = FastAPI()
 
 
+GameID = UUID
+PlayerID = UUID
+WebSocketID = UUID
+
+
+class Player(BaseModel):
+    id: UUID
+    color: Color
+
+
 class GameWrapper:
     def __init__(self, onitama_game: Onitama):
         self.game = onitama_game
-        self.num_players = 0
-        self.player_colors = {}
+        self.players: Dict[PlayerID, Player] = {}
         self.available_colors = list(Color)
 
-    def add_player(self, player_id: str, color: Color):
-        self.player_colors[player_id] = color
-        self.available_colors.remove(color)
-
-    def get_player_color(self, player_id: str) -> Color:
-        return self.player_colors.get(player_id)
+    def add_player(self, player: Player):
+        self.players[player.id] = player
+        self.available_colors.remove(player.color)
 
     def assign_random_color(self) -> Color:
         return random.choice(self.available_colors)
 
+    @property
+    def is_full(self):
+        return len(self.players) >= 2
+
 
 class GameManager:
     def __init__(self):
-        self.games: Dict[str, GameWrapper] = {}
+        self.games: Dict[GameID, GameWrapper] = {}
 
-    def create_game(self) -> str:
-        game_id = str(uuid4())
+    def create_game(self) -> GameID:
+        game_id = uuid4()
         game = Onitama()
         game_wrapper = GameWrapper(game)
         self.games[game_id] = game_wrapper
         return game_id
 
-    def join_game(self, game_id: str) -> Color:
+    def join_game(self, game_id: GameID) -> Player:
         game_wrapper = self.games.get(game_id)
         if not game_wrapper:
             raise HTTPException(status_code=404, detail="Game not found")
 
-        if game_wrapper.num_players >= 2:
+        if game_wrapper.is_full:
             raise HTTPException(status_code=400, detail="Game is already full")
 
-        game_wrapper.num_players += 1
-        player_color = game_wrapper.assign_random_color()
-        game_wrapper.add_player(game_id, player_color)
+        player = Player(id=uuid4(), color=game_wrapper.assign_random_color())
+        game_wrapper.add_player(player)
 
-        return player_color
+        return player
 
-    def get_game(self, game_id: str) -> Onitama:
+    def get_game(self, game_id: GameID) -> Onitama:
         game_wrapper = self.games.get(game_id)
         if not game_wrapper:
             raise HTTPException(status_code=404, detail="Game not found")
 
         return game_wrapper.game
 
-    def remove_game(self, game_id: str):
+    def remove_game(self, game_id: GameID):
         if game_id in self.games:
             del self.games[game_id]
 
@@ -74,18 +85,18 @@ game_manager = GameManager()
 
 class WebSocketManager:
     def __init__(self):
-        self.active_connections: Dict[str, List[WebSocket]] = {}
+        self.active_connections: Dict[WebSocketID, List[WebSocket]] = {}
 
-    async def connect(self, websocket: WebSocket, game_id: str):
+    async def connect(self, websocket: WebSocket, game_id: WebSocketID):
         await websocket.accept()
         if game_id not in self.active_connections:
             self.active_connections[game_id] = []
         self.active_connections[game_id].append(websocket)
 
-    async def disconnect(self, websocket: WebSocket, game_id: str):
+    async def disconnect(self, websocket: WebSocket, game_id: WebSocketID):
         self.active_connections[game_id].remove(websocket)
 
-    async def broadcast_game_state(self, game_id: str, game_state: dict):
+    async def broadcast_game_state(self, game_id: WebSocketID, game_state: dict):
         if game_id in self.active_connections:
             for websocket in self.active_connections[game_id]:
                 await websocket.send_json(game_state)
@@ -97,27 +108,27 @@ websocket_manager = WebSocketManager()
 @app.post("/create_game")
 def create_game():
     game_id = game_manager.create_game()
-    player_color = game_manager.join_game(game_id)
+    player = game_manager.join_game(game_id)
 
-    return {"game_id": game_id, "color": player_color.name}
+    return {"game_id": game_id, "player": player}
 
 
 @app.post("/join_game/{game_id}")
-def join_game(game_id: str):
-    player_color = game_manager.join_game(game_id)
+def join_game(game_id: GameID):
+    player = game_manager.join_game(game_id)
 
-    return {"game_id": game_id, "color": player_color.name}
+    return {"game_id": game_id, "player": player}
 
 
 @app.get("/game_state/{game_id}")
-def game_state(game_id: str):
+def game_state(game_id: GameID):
     game = game_manager.get_game(game_id)
 
     return game.get_game_state()
 
 
 @app.websocket("/ws/{game_id}")
-async def websocket_endpoint(websocket: WebSocket, game_id: str):
+async def websocket_endpoint(websocket: WebSocket, game_id: GameID):
     game = game_manager.get_game(game_id).game
     if not game:
         return
@@ -164,3 +175,7 @@ async def websocket_endpoint(websocket: WebSocket, game_id: str):
         game_manager.remove_game(game_id)
         if game_id in websocket_manager.active_connections:
             del websocket_manager.active_connections[game_id]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
